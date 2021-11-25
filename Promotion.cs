952b510bc8a2d72502efc4f3816e7f4d02b011a2
cwfietz <cwfietz@calcna.ab.ca>
2021-11-25 00:48:52 -0700

    Add empty Promotion

--- Promotion.cs
+++ Promotion.cs
@@ -1,17 +1,48 @@
-﻿namespace PrototypeCheckoutSystem
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PrototypeCheckoutSystem
 {
+    public enum TypeOfPromotion
+    {
+        OnSale,
+        GroupQuantity,
+        AdditionalDiscount
+    }
+
     public class Promotion
     {
-        public string descriptor;
+        public string ProductName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; } 
+        public TypeOfPromotion promotionType;
+        public MoneyAmount EffectivePrice { get; set; } // for OnSale or GroupQuantity
+        public int BuyQuantity { get; set; } // for GroupQuantity or AdditionalDiscount
+        public int DiscountedQuantity { get; set; } // how many are discounted to effectivePrice or discountProportion
+        public decimal DiscountProportion { get; set; } // for AdditionalDiscount
+        public string Descriptor { get; set; } // note to add to the receipt
 
         public Promotion()
         {
-            this.descriptor = "";
+            ProductName = "";
+            StartDate = new DateTime(2021, 11, 24);
+            EndDate = new DateTime(2021, 11, 25);
+            promotionType = TypeOfPromotion.OnSale;
+            EffectivePrice = new MoneyAmount();
+            BuyQuantity = 0;
+            DiscountedQuantity = 0;
+            DiscountProportion = 0.0m;
+            Descriptor = "";
+        }
+
+        public string PromotionToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented) ;
         }
 
         public override string ToString()
         {
-            return descriptor.ToString();
+            return "";
         }
     }
 }
--- PromotionTests.cs
+++ PromotionTests.cs
@@ -1,14 +1,15 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 namespace PrototypeCheckoutSystem
 {
     [TestFixture()]
     public class PromotionTests
     {
         [Test()]
-        public void CreatePromotion()
+        public void CreateEmptyPromotion()
         {
             var testPromotion = new Promotion();
-            Assert.AreEqual("", testPromotion.ToString());
+            Assert.AreEqual("{\n  \"promotionType\": 0,\n  \"ProductName\": \"\",\n  \"StartDate\": \"2021-11-24T00:00:00\",\n  \"EndDate\": \"2021-11-25T00:00:00\",\n  \"EffectivePrice\": {\n    \"Symbol\": \"$\",\n    \"Amount\": 0.00\n  },\n  \"BuyQuantity\": 0,\n  \"DiscountedQuantity\": 0,\n  \"DiscountProportion\": 0.0,\n  \"Descriptor\": \"\"\n}", testPromotion.PromotionToString());
         }
     }
 }
